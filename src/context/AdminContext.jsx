9ad7af45f2bd91748cf65f3155dbddcd37790f87
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-05-12 20:18:08 +0530

    add changeAvaialbility fn in UI

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import axios from 'axios';
-import toast from 'react-toastify';
+import { toast } from 'react-toastify';
 
 export const AdminContext = createContext()
 
@@ -15,9 +15,23 @@ const AdminContextProvider = (props) => {
 
         try {
             const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, { headers: { aToken } })
-            if(data.success){
+            if (data.success) {
                 setDoctors(data.doctors)
                 console.log(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const changeAvailability = async (docId) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, { headers: { aToken } })
+            if(data.success){
+                toast.success(data.message)
+                getAllDoctors()
             }else{
                 toast.error(data.message)
             }
@@ -28,7 +42,7 @@ const AdminContextProvider = (props) => {
 
     const value = {
         aToken, setAToken,
-        backendUrl,doctors,getAllDoctors
+        backendUrl, doctors, getAllDoctors,changeAvailability
     }
 
     return (
