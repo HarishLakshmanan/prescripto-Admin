40acd8e1b5dc3b2834a2fe67b2127a3f7f703ecd
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-06-14 17:51:40 +0530

    add admin functionality

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,17 +2,40 @@ import { createContext } from "react";
 
 export const AppContext = createContext()
 
-const AppContextProvider = (props)=>{
-  
-    const value = {
+const AppContextProvider = (props) => {
 
-    }
 
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
+  const currency = '₹'
+
+  const calculateAge = (dob) => {
+    const today = new Date()
+    const birthday = new Date(dob)
+
+    let age = today.getFullYear() - birthday.getFullYear()
+    return age
+  }
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "sep", "Oct", "Nov", "Dec"]
+
+
+  const slotDateFormet = (slotDate) => {
+
+    const dateArray = slotDate.split('_');
+    const monthIndex = Number(dateArray[1]);
+
+
+    return `${dateArray[0]} ${months[monthIndex]} ${dateArray[2]}`;
+  };
+
+  const value = {
+    calculateAge, slotDateFormet,currency
+  }
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  )
 }
 
 export default AppContextProvider
