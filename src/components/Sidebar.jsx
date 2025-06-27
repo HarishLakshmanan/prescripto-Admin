7bf0de7d88a936fa462f80b0f80f52c8f4c1aacf
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-06-27 13:58:40 +0530

    add functionality to doctorpanel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,22 +16,22 @@ export default function Sidebar() {
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/admin-dashboard'}>
                         <img src={assets.home_icon} />
-                        <p>Dashboard</p>
+                        <p className='hidden md:block'>Dashboard</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/all-appointment'}>
                         <img src={assets.appointment_icon} />
-                        <p>Appointments</p>
+                        <p className='hidden md:block'>Appointments</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/add-doctor'}>
                         <img src={assets.add_icon} />
-                        <p>Add Doctor</p>
+                        <p className='hidden md:block'>Add Doctor</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/doctor-list'}>
                         <img src={assets.people_icon} />
-                        <p>Doctors List</p>
+                        <p className='hidden md:block'>Doctors List</p>
                     </NavLink>
 
 
@@ -43,17 +43,17 @@ export default function Sidebar() {
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/doctor-dashboard'}>
                         <img src={assets.home_icon} />
-                        <p>Dashboard</p>
+                        <p className='hidden md:block'>Dashboard</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/doctor-appointment'}>
                         <img src={assets.appointment_icon} />
-                        <p>Appointments</p>
+                        <p className='hidden md:block'>Appointments</p>
                     </NavLink>
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/doctor-profile'}>
                         <img src={assets.people_icon} />
-                        <p>profile</p>
+                        <p className='hidden md:block'>profile</p>
                     </NavLink>
                 </ul>
             }
--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createContext } from "react";
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import axios from 'axios'
 
 export const DoctorContext = createContext()
@@ -11,14 +11,62 @@ const DoctorContextProvider = (props) => {
 
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
 
     const getAppointments = async () => {
         try {
             const { data } = await axios.get(backendUrl + '/api/doctor/appointments', { headers: { dToken } })
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments);
+
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(data.message)
+        }
+    }
+
+    const completeAppointment = async (appointmentId) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/doctor/appointment-completed', { appointmentId }, { headers: { dToken } })
             if(data.success){
-                setAppointments(data.appointments.reversr())
-                console.log(data.appointments.reversr());
-                
+                toast.success(data.message)
+                getAppointments()
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(data.message)
+        }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/doctor/appointment-cancelled', { appointmentId }, { headers: { dToken } })
+            if(data.success){
+                toast.success(data.message)
+                getAppointments()
+            }else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(data.message)
+        }
+    }
+
+    const getDashData = async()=>{
+        try {
+            const {data} = await axios.get(backendUrl+'/api/doctor/dashboard',{headers:{dToken}})
+
+            if(data.success){
+                setDashData(data.dashData)
             }else{
                 toast.error(data.message)
             }
@@ -29,7 +77,8 @@ const DoctorContextProvider = (props) => {
     }
 
     const value = {
-        dToken, setDToken, backendUrl,appointments,setAppointments,getAppointments
+        dToken, setDToken, backendUrl, appointments, setAppointments, getAppointments,completeAppointment,cancelAppointment,
+        dashData,setDashData,getDashData
     }
 
     return (
